fc2e7086e82dc407755b19e925c52f59a5e4bb67
wonkeeee <wonk0312@naver.com>
2024-09-21 00:55:36 +0900

    SRR 적용하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,43 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { ReactNode } from "react";
-import movies from "@/mock/dummy.json";
 import MovieItem from "@/components/movie-item";
 import style from "./index.module.css";
+import fetchMovies from "@/lib/fetch-movies";
+import fetchRandomMovies from "@/lib/fetch-random-movies";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+export const getServerSideProps = async () => {
+  const [allMovies, recoMovies] = await Promise.all([
+    fetchMovies(),
+    fetchRandomMovies(),
+  ]);
+
+  return {
+    props: {
+      allMovies,
+      recoMovies,
+    },
+  };
+};
+
+export default function Home({
+  allMovies,
+  recoMovies,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section className={style.recommend_section}>
         <h2>지금 가장 추천하는 영화</h2>
         <div className={style.poster_img_wrap}>
-          {movies.slice(0, 3).map((movie) => (
-            <MovieItem key={`${movie.id}`} {...movie} />
+          {recoMovies.map((movie) => (
+            <MovieItem key={movie.id} {...movie} />
           ))}
         </div>
       </section>
       <section className={style.all_movie_section}>
         <h2>등록된 모든 영화</h2>
         <div className={style.poster_img_wrap}>
-          {movies.map((movie) => (
+          {allMovies.map((movie) => (
             <MovieItem key={movie.id} {...movie} />
           ))}
         </div>
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,11 +1,27 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
-import movies from "@/mock/dummy.json";
 import MovieItem from "@/components/movie-item";
 import style from "./search.module.css";
+import fetchMovies from "@/lib/fetch-movies";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 
-export default function Page() {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const q = context.query.q;
+  const movies = await fetchMovies(q as string);
+
+  return {
+    props: {
+      movies,
+    },
+  };
+};
+
+export default function Page({
+  movies,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       {movies.map((movie) => (
