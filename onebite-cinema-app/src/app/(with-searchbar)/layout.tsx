4081f0e2c34a9ed59222a387792fffd1ce622e42
wonke-dev <wonk0311@gmail.com>
2024-10-01 23:58:10 +0900

    서치바 컴포넌트 만들기

--- onebite-cinema-app/src/app/(with-searchbar)/layout.tsx
+++ onebite-cinema-app/src/app/(with-searchbar)/layout.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from "react";
+import Searchbar from "../components/searchbar";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
       <div>
-        Searchbar Layout
+        <Searchbar />
         {children}
       </div>
     </div>
